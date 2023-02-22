e9abf74d0cc2c09f53b5c179bc5d55501c047631
aimeepham14 <aimee.pham14@gmail.com>
2023-02-21 17:54:48 -0800

    started benefits page

---
+++ gym-typescript/src/scenes/benefits/index.tsx
@@ -0,0 +1,66 @@
+import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from "@heroicons/react/24/solid";
+import { BenefitType, SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion"
+import HText from "@/shared/HText";
+import React from "react";
+import Benefit from "./Benefit";
+
+const benefits: Array<BenefitType> = [
+    {
+        icon: <HomeModernIcon className="h-6 w-6" />,
+        title: "State of the Art Facilities",
+        description:
+            "Add a description here"
+    },
+    {
+        icon: <UserGroupIcon className="h-6 w-6" />,
+        title: "100's of Diverse Classes",
+        description:
+            "Add a description here"
+    },
+    {
+        icon: <AcademicCapIcon className="h-6 w-6" />,
+        title: "Export and Pro Trainers",
+        description:
+            "Add a description here"
+    }
+]
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Benefits = ( {setSelectedPage}: Props) => {
+    return <section
+        id="benefits"
+        className="mx-auto min-h-full w-5/6 py-20"
+    >
+        <motion.div
+            onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+        >
+            <div className="md:my-5 md:w-3/5">
+                <HText>MORE THAN JUST A GYM.</HText>
+                <p className="my-5 text-sm">
+                    We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide true care into each and every member.
+                </p>
+            </div>
+
+            {/* BENEFITS */}
+            <div className="md:flex items-center justify-between gap-8 mt-5">
+                {benefits.map((benefit: BenefitType) => (
+                    <Benefit 
+                        key={benefit.title}
+                        icon={benefit.icon}
+                        title={benefit.title}
+                        description={benefit.description}
+                        setSelectedPage={setSelectedPage}
+                    />
+                ))}
+            </div>
+        </motion.div>
+
+    </section>;
+
+}; 
+
+export default Benefits;
