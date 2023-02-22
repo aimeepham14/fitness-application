7e3193de480e9efcd40da1a679034efa461bc17c
aimeepham14 <aimee.pham14@gmail.com>
2023-02-22 12:19:21 -0800

    added animations to description page

--- gym-typescript/src/scenes/benefits/index.tsx
+++ gym-typescript/src/scenes/benefits/index.tsx
@@ -91,27 +91,45 @@ const Benefits = ( {setSelectedPage}: Props) => {
                     className="mx-auto"
                     alt="benefits-page-graphic"
                     src={BenefitsPageGraphic} 
-                />
+                /> 
                 
                 {/* DESCRIPTION */}
                 <div>
                     {/* TITLE */}
                     <div className="relative">
                         <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] before:content-abstractwaves">
-                            <div className="relative">
+                            <motion.div
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ once: true, amount: 0.5 }}
+                                transition={{ delay: 0.2, duration: 0.5 }}
+                                variants={{
+                                    hidden: { opacity: 0, x: 50 },
+                                    visible: { opacity: 1, x: 0 },
+                                }}
+                            >
                                 <HText>
                                     MILLIONS OF HAPPY MEMBERS GETTING{" "}
                                     <span className="text-primary-500">FIT</span>
                                 </HText>
-                            </div>
+                            </motion.div>
                         </div>
                     </div>
 
                     {/* DESCRIPTION */}
-                    <div>
+                    <motion.div
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: 50 },
+                            visible: { opacity: 1, x: 0 },
+                        }}
+                    >
                         <p className="my-5">Add a main description here</p>
                         <p className="mb-5">Add another main description here</p>
-                    </div>
+                    </motion.div>
 
                     {/* BUTTON */}
                     <div className="relative mt-16">
