af2d26525740c85429e29f7eacb0f990f083e508
aimeepham14 <aimee.pham14@gmail.com>
2023-02-21 16:36:02 -0800

    added framer motion to heading

--- gym-typescript/src/scenes/home/index.tsx
+++ gym-typescript/src/scenes/home/index.tsx
@@ -7,6 +7,7 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from 'framer-motion';
 
 
 type Props = {
@@ -25,22 +26,32 @@ const Home = ({setSelectedPage }: Props) => {
             {/* MAIN HEADER */}
             <div className="z-10 mt-32 md:basis-3/5">
                 {/* HEADINGS */}
-                <div>
-                    <div>
-                        <div>
+                <motion.div 
+                    className="md:mt-20"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x:-50},
+                        visible: { opacity: 1, x: 0 },
+                    }}
+                >
+                    <div className="relative">
+                        <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                             <img alt="home-page-text" src={HomePageText} />
                         </div>
                     </div>
 
-                    <p>
+                    <p className="mt-8 text-sm">
                        There's no better time than now to kick-start your fitness routine. Run, jump, squat or spin - workout how you want with No Judgements! 
                     </p>
-                </div>
+                </motion.div>
 
                 {/* ACTIONS */}
-                <div>
+                <div className="mt-8 flex items-center gap-8">
                     <ActionButton setSelectedPage={setSelectedPage}>
-                        Join Now
+                        Join Now 
                     </ActionButton>
                     <AnchorLink
                         className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
@@ -53,23 +64,23 @@ const Home = ({setSelectedPage }: Props) => {
             </div>
 
             {/* IMAGE */}
-            <div>
+            <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
                 <img alt="home-pageGraphic" src={HomePageGraphic} />
             </div>
         </div>
 
         {/* SPONSERS */}
         {isAboveMediumScreens && (
-            <div>
-                <div>
-                    <div>
+            <div className="h-[150px] w-full bg-primary-100 py-10">
+                <div className="mx-auto w-5/6">
+                    <div className="flex w-3/5 items-center justify-between gap-8">
                         <img alt="redbull-sponsor" src={SponsorRedBull} />
                         <img alt="forbes-sponsor" src={SponsorForbes} />
                         <img alt="fortune-sponsor" src={SponsorFortune} />
                     </div>
                 </div>
             </div>
-        )}
+        )} 
     </section>
 }
 
